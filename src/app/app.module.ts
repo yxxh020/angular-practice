593a7b00ac9b9a3e7995b1cb6d6a86482bdd3876
yxxh020 <108642131+yxxh020@users.noreply.github.com>
2023-01-20 11:07:39 +0900

    2 files modified, 13 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,23 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+import { TimeDisplayComponent } from './section/time-display/time-display.component';
+import { ButtonsComponent } from './section/buttons/buttons.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    FooterComponent, 
+    SectionComponent,
+    TimeDisplayComponent,
+    ButtonsComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
